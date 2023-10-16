8896ef6e2c9123cd17490e8d0fbbb0dd840be9d5
VDA <Vasilenkoda91@gmail.com>
2023-10-16 22:50:56 +0300

    New function and modification in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,6 +1,13 @@
 def do_capital(str):
+
+    """Делает все буквы заглавными"""
+
     return str.upper()
 
 
+def do_capital_first(str):
 
+    """Делает первую букву заглавной"""
+
+    return str.title()
 
