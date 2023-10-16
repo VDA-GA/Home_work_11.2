9f08d7beb109cf79c11f4d65a6d8c8b108c7c660
VDA <Vasilenkoda91@gmail.com>
2023-10-16 22:29:34 +0300

    New function and modification in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,5 +1,14 @@
 def do_capital(str):
+    '''Делает буквы строчными'''
     return str.upper()
+
+
+def do_capital_first_letter(str):
+    '''Делает строчной первую букву'''
+    return str.title()
+
+
+
 
 
 
